f9d82ed940000f8c31a48da51d0d472dd7e19818
Simone Morlacchini <smorlacchini@pallexitalia.it>
2022-10-22 15:47:28 +0200

    Update CORS header

--- api/app.js
+++ api/app.js
@@ -23,6 +23,9 @@ MongoDB.connect();
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
   next();
 });
 
