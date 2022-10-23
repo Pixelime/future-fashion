96d9e310d8bf4858ea0b2d6e1127c2d4204e1bbb
Simone Morlacchini <smorlacchini@pallexitalia.it>
2022-10-23 08:35:11 +0200

    Better Mongoose Schema validation

--- api/models/User.js
+++ api/models/User.js
@@ -5,18 +5,19 @@ const UserSchema = new Schema({
     firstname: {
         type: String,
         trim: true,
-        required: true,
+        required: [true, 'a value is required'],
         index: true
     },
     lastname: {
         type: String,
         trim: true,
-        required: true,
+        required: [true, 'a value is required'],
         index: true
     },
     entries: {
         type: Number,
-        required: true,
+        required: [true, 'a value greater than 0 is required'],
+        validate: [(val) => val > 0, 'a value greater than 0 is required'],
         default: 10
     },
     lastEntry: {
